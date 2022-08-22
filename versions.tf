9a1d623bc83310a0aa7f77336bcb773db02a6a00
Sandro Flausino <sjflausino@gmail.com>
2022-08-22 09:20:50 -0300

    adicionando terraforms e workflows

---
+++ versions.tf
@@ -0,0 +1,11 @@
+terraform {
+  experiments      = [module_variable_optional_attrs]
+  required_version = ">= 0.13.1"
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.2.0"
+    }
+  }
+}
